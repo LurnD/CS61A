72bc69ab09264facefc279574ad85c203dc8f983
LurnD <a792151034@gmail.com>
2022-02-15 13:20:02 +0800

    coswitch to cs61a-fa21 version & complete lab00

--- labs/lab00/lab00.py
+++ labs/lab00/lab00.py
@@ -1,8 +1,8 @@
-def twenty_twenty_two():
-    """Come up with the most creative expression that evaluates to 2022,
+def twenty_twenty_one():
+    """Come up with the most creative expression that evaluates to 2021,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_twenty_two()
-    2022
+    >>> twenty_twenty_one()
+    2021
     """
-    return ______
+    return 2 * 1000 + 22 - 1
